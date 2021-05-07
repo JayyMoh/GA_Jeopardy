5c4e7826651f11fd812dff3cd84e0d941ae1c45c
Jonathan <jonmohr@outlook.com>
2021-05-07 11:34:18 -0400

    All board item divs have been stored as varaibles

--- app.js
+++ app.js
@@ -1,1 +1,48 @@
 console.log('JS is connected')
+
+// Variables for the Category Boxes
+const cat_1 = document.querySelector('.cat-1')
+const cat_2 = document.querySelector('.cat-2')
+const cat_3 = document.querySelector('.cat-3')
+const cat_ = document.querySelector('.cat-4')
+const cat_5 = document.querySelector('.cat-5')
+
+// Variables for Category 1s Question Boxes 
+const cat_1_100 = document.querySelector('.cat-1-100')
+const cat_1_200 = document.querySelector('.cat-1-200')
+const cat_1_300 = document.querySelector('.cat-1-300')
+const cat_1_400 = document.querySelector('.cat-1-400')
+const cat_1_500 = document.querySelector('.cat-1-500')
+
+// Variables for Category 2s Question Boxes 
+const cat_2_100 = document.querySelector('.cat-2-100')
+const cat_2_200 = document.querySelector('.cat-2-200')
+const cat_2_300 = document.querySelector('.cat-2-300')
+const cat_2_400 = document.querySelector('.cat-2-400')
+const cat_2_500 = document.querySelector('.cat-2-500')
+
+// Variables for Category 3s Question Boxes 
+const cat_3_100 = document.querySelector('.cat-3-100')
+const cat_3_200 = document.querySelector('.cat-3-200')
+const cat_3_300 = document.querySelector('.cat-3-300')
+const cat_3_400 = document.querySelector('.cat-3-400')
+const cat_3_500 = document.querySelector('.cat-3-500')
+
+// Variables for Category 4s Question Boxes 
+const cat_4_100 = document.querySelector('.cat-4-100')
+const cat_4_200 = document.querySelector('.cat-4-200')
+const cat_4_300 = document.querySelector('.cat-4-300')
+const cat_4_400 = document.querySelector('.cat-4-400')
+const cat_4_500 = document.querySelector('.cat-4-500')
+
+// Variables for Category 5s Question Boxes 
+const cat_5_100 = document.querySelector('.cat-5-100')
+const cat_5_200 = document.querySelector('.cat-5-200')
+const cat_5_300 = document.querySelector('.cat-5-300')
+const cat_5_400 = document.querySelector('.cat-5-400')
+const cat_5_500 = document.querySelector('.cat-5-500')
+
+// Create the Board Object that stores all of the variables in a way we can itterate throguh
+
+
+
