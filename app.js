1005e9911800d0b89363a29a357a69e55190bb3b
Jonathan <jonmohr@outlook.com>
2021-05-07 12:19:26 -0400

    For loop itterates through the Board array properly

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ console.log('JS is connected')
 const cat_1 = document.querySelector('.cat-1')
 const cat_2 = document.querySelector('.cat-2')
 const cat_3 = document.querySelector('.cat-3')
-const cat_ = document.querySelector('.cat-4')
+const cat_4 = document.querySelector('.cat-4')
 const cat_5 = document.querySelector('.cat-5')
 
 // Variables for Category 1s Question Boxes 
@@ -42,7 +42,51 @@ const cat_5_300 = document.querySelector('.cat-5-300')
 const cat_5_400 = document.querySelector('.cat-5-400')
 const cat_5_500 = document.querySelector('.cat-5-500')
 
-// Create the Board Object that stores all of the variables in a way we can itterate throguh
+// Create the Board array
+const jeopardyBoard = [
+    cat_1, 
+    cat_1_100,
+    cat_1_200,
+    cat_1_300,
+    cat_1_400,
+    cat_1_500,
+    cat_2,
+    cat_2_100,
+    cat_2_200,
+    cat_2_300,
+    cat_2_400,
+    cat_2_500,
+    cat_3,
+    cat_3_100,
+    cat_3_200,
+    cat_3_300,
+    cat_3_400,
+    cat_3_500,
+    cat_4,
+    cat_4_100,
+    cat_4_200,
+    cat_4_300,
+    cat_4_400,
+    cat_4_500,
+    cat_5,
+    cat_5_100,
+    cat_5_200,
+    cat_5_300,
+    cat_5_400,
+    cat_5_500,
+]
 
+const startGame = () => {
+    for (i = 0; i < jeopardyBoard.length; i++) {
+        jeopardyBoard[i].addEventListener('click', (e) => {
+            if (e.currentTarget === cat_5_500) {
+                console.log('You have selected Category 5, for 500 Points')
+            } else {
+                console.log('That is not cat 5')
+            }
+        })
+    }
+     
+}
 
-
+startGame()
