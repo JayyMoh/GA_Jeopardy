abf29308ef996b33ba068724522e3df7045d0258
Jonathan <jonmohr@outlook.com>
2021-05-11 15:13:48 -0400

    Object IDs reformatted

--- app.js
+++ app.js
@@ -48,151 +48,151 @@ const restartBtn = document.getElementById('restart-button')
 
 
 const questionObject = {
-        Q1: {   
+        sports1: {   
                 category: 'sports',
                 value: 100, 
                 question: 'What company\'s logo is a swoosh?', 
                 answer: 'Nike',
         },    
-        Q2: {
+        sports2: {
                 category: 'sports',
                 value: 200, 
                 question: 'What do you call it when a bowler makes three strikes in a row?', 
                 answer: 'Turkey',
         },
-        Q3: {
+        sports3: {
                 category: 'sports',
                 value: 300, 
                 question: 'The Olympics are held every how many years?', 
                 answer: 'Four',
         },
-        Q4: {
+        sports4: {
                 category: 'sports',
                 value: 400, 
                 question: 'What is the national sport of Canada?', 
                 answer: 'Lacrosse',
         },
-        Q5: {
+        sports5: {
                 category: 'sports',
                 value: 500, 
                 question: 'Who has won more tennis grand slam titles, Venus Williams or Serena Williams?',
                 answer: 'Serena Williams'
         },
-        Q6: {
+        fnD1: {
                 category: 'Food & Drink',
                 value: 100,
                 question: 'What is the national spirit of America?',
                 answer: 'Bourbon',
         },
-        Q7: {
+        fnD2: {
                 category: 'Food & Drink',
                 value: 200,
                 question: 'What is the most iconic food item in Chicago city?',
                 answer: 'Deep-dish Pizza',
         },
-        Q8: {
+        fnD3: {
                 category: 'Food & Drink',
                 value: 300,
                 question: 'Name the main ingredient of mince-pie.',
                 answer: 'Dried Fruit',
         },
-        Q9: {
+        fnD4: {
                 category: 'Food & Drink',
                 value: 400,
                 question: 'What is the national dish of Germany?',
                 answer: 'Pot Roast',
         },
-        Q10: {
+        fnD5: {
                 category: 'Food & Drink',
                 value: 500,
                 question: 'Name the national drink of Scotland other than whiskey.',
                 answer: 'Irn-Bru',
         },
-        Q11: {
+        theatre1: {
                 category: 'Theatre',
                 value: 100,
                 question: 'The majority of Broadway shows are what?',
                 answer: 'Musicals',
         },
-        Q12: {
+        theatre2: {
                 category: 'Theatre',
                 value: 200,
                 question: 'Which Oscar-winning musical is getting a 2020 remake courtesy of Steven Spielberg?',
                 answer: 'West Side Story',
         },
-        Q13: {
+        theatre3: {
                 category: 'Theatre',
                 value: 300,
                 question: 'In Shakespeare’s King Lear, which of his three daughters really does love him and is true to him?',
                 answer: 'Cordelia',
         },
-        Q14: {
+        theatre4: {
                 category: 'Theatre',
                 value: 400,
                 question: 'The Bowery Theatre opened in what year?',
                 answer: '1826',
         },
-        Q15: {
+        theatre5: {
                 category: 'Theatre',
                 value: 500,
                 question: 'Beginning with The Red Mill, Broadway shows installed what outside the theatres?',
                 answer: 'Electric signs',
         },
-        Q16: {
+        music1: {
                 category: 'Music',
                 value: 100,
                 question: 'What was Elvis Presley\'s first hit in 1956?',
                 answer: 'Heartbreak Hotel',
         },
-        Q17: {
+        music2: {
                 category: 'Music',
                 value: 200,
                 question: 'Which famous music group was formerly known as the New Yardbirds?',
                 answer: 'Led Zeppelin',
         },
-        Q18: {
+        music3: {
                 category: 'Music',
                 value: 300,
                 question: 'Which singer is a godmother to Elton John\'s two sons?',
                 answer: 'Lady Gaga',
         },
-        Q19: {
+        music4: {
                 category: 'Music',
                 value: 400,
                 question: 'The Rock and Roll Hall of Fame is situated in what US State?',
                 answer: 'Ohio',
         },
-        Q20: {
+        music5: {
                 category: 'Music',
                 value: 500,
                 question: 'Which pop singer\'s real name is Robyn Fenty?',
                 answer: 'Rihanna',
         },
-        Q21: {
+        flw1: {
             category: 'Four Letter Words',
             value: 100,
             question: 'The triangular frame used to line up the billiard balls at the start of the game.',
             answer: 'Rack',
         },
-        Q22: {
+        flw2: {
                 category: 'Four Letter Words',
                 value: 200,
                 question: 'This word commonly follows cuff or missing. ',
                 answer: 'Link',
         },
-        Q23: {
+        flw3: {
                 category: 'Four Letter Words',
                 value: 300,
                 question: 'From the Greek word for "deep sleep", it\'s a deep, prolonged unconsciousness.',
                 answer: 'Coma',
         },
-        Q24: {
+        flw4: {
                 category: 'Four Letter Words',
                 value: 400,
                 question: 'The dry particles of matter found on your furniture or what you do to remove them.',
                 answer: 'Dust',
         },
-        Q25: {
+        flw5: {
                 category: 'Four Letter Words',
                 value: 500,
                 question: 'The first name isn\'t on the "Keystone" of the house, but that of the Sennett.',
@@ -213,97 +213,99 @@ const closeModal = () => {
 closeBtn.addEventListener('click', closeModal)
 
 
-let click = 0
 let currentScore = 0
 const startGame = () => {
     
     document.querySelectorAll('.question-box').forEach((box) => {
         box.addEventListener('click', (e) => {
-            click++
-            if (e.currentTarget === sports_100) {
-                sports_100.innerText = ''
-                modal_question.innerText = questionObject.Q1.question
-                let answer = questionObject.Q1.answer
-                let value = questionObject.Q1.value
-                openModal()
-                submitBtn.addEventListener('click', () => {
-                    let playerAnswer = document.getElementById('player-answer').value
-                    if (playerAnswer === answer) {
-                        modal_question.innerText = 'That\'s Correct!'
-                        currentScore = currentScore + value
-                        scoreBoard.innerText = 'Score: ' + currentScore
-                    } else {
-                        modal_question.innerText = 'That\'s Incorrect. The correct answer is ' + '"' + answer + '".'
-                    }
-                })
-            } else if (e.currentTarget === sports_200) {
-                sports_200.innerText = ''
-                modal_question.innerText = questionObject.Q2.question
-                let answer = questionObject.Q2.answer
-                let value = questionObject.Q2.value
-                openModal()
-                submitBtn.addEventListener('click', () => {
-                    let playerAnswer = document.getElementById('player-answer').value
-                    if (playerAnswer === answer) {
-                        modal_question.innerText = 'That\'s Correct!'
-                        currentScore = currentScore + value
-                        scoreBoard.innerText = 'Score: ' + currentScore
-                    } else {
-                        modal_question.innerText = 'That\'s Incorrect. The correct answer is ' + '"' + answer + '".'
-                    }
-                })
-            } else if (e.currentTarget === sports_300) {
-                sports_300.innerText = ''
-                modal_question.innerText = questionObject.Q3.question
-                let answer = questionObject.Q3.answer
-                let value = questionObject.Q3.value
-                openModal()
-                submitBtn.addEventListener('click', () => {
-                    let playerAnswer = document.getElementById('player-answer').value
-                    if (playerAnswer === answer) {
-                        modal_question.innerText = 'That\'s Correct!'
-                        currentScore = currentScore + value
-                        scoreBoard.innerText = 'Score: ' + currentScore
-                    } else {
-                        modal_question.innerText = 'That\'s Incorrect. The correct answer is ' + '"' + answer + '".'
-                    }
-                })
-            } else if (e.currentTarget === sports_400) {
-                sports_400.innerText = ''
-                modal_question.innerText = questionObject.Q4.question
-                let answer = questionObject.Q4.answer
-                let value = questionObject.Q4.value
-                openModal()
-                submitBtn.addEventListener('click', () => {
-                    let playerAnswer = document.getElementById('player-answer').value
-                    if (playerAnswer === answer) {
-                        modal_question.innerText = 'That\'s Correct!'
-                        currentScore = currentScore + value
-                        scoreBoard.innerText = 'Score: ' + currentScore
-                    } else {
-                        modal_question.innerText = 'That\'s Incorrect. The correct answer is ' + '"' + answer + '".'
-                    }
-                })
-            } else if (e.currentTarget === sports_500) {
-                sports_500.innerText = ''
-                modal_question.innerText = questionObject.Q5.question
-                let answer = questionObject.Q5.answer
-                let value = questionObject.Q5.value
-                openModal()
-                submitBtn.addEventListener('click', () => {
-                    let playerAnswer = document.getElementById('player-answer').value
-                    if (playerAnswer === answer) {
-                        modal_question.innerText = 'That\'s Correct!'
-                        currentScore = currentScore + value
-                        scoreBoard.innerText = 'Score: ' + currentScore
-                    } else {
-                        modal_question.innerText = 'That\'s Incorrect. The correct answer is ' + '"' + answer + '".'
-                    }
-                })
-            }
+
+            
+            console.log(questionObject[e.currentTarget.id])
+
+            // if (e.currentTarget === sports_100) {
+            //     sports_100.innerText = ''
+            //     modal_question.innerText = questionObject.Q1.question
+            //     let answer = questionObject.Q1.answer
+            //     let value = questionObject.Q1.value
+            //     openModal()
+            //     submitBtn.addEventListener('click', () => {
+            //         let playerAnswer = document.getElementById('player-answer').value
+            //         if (playerAnswer === answer) {
+            //             modal_question.innerText = 'That\'s Correct!'
+            //             currentScore = currentScore + value
+            //             scoreBoard.innerText = 'Score: ' + currentScore
+            //         } else {
+            //             modal_question.innerText = 'That\'s Incorrect. The correct answer is ' + '"' + answer + '".'
+            //         }
+            //     })
+            // } else if (e.currentTarget === sports_200) {
+            //     sports_200.innerText = ''
+            //     modal_question.innerText = questionObject.Q2.question
+            //     let answer = questionObject.Q2.answer
+            //     let value = questionObject.Q2.value
+            //     openModal()
+            //     submitBtn.addEventListener('click', () => {
+            //         let playerAnswer = document.getElementById('player-answer').value
+            //         if (playerAnswer === answer) {
+            //             modal_question.innerText = 'That\'s Correct!'
+            //             currentScore = currentScore + value
+            //             scoreBoard.innerText = 'Score: ' + currentScore
+            //         } else {
+            //             modal_question.innerText = 'That\'s Incorrect. The correct answer is ' + '"' + answer + '".'
+            //         }
+            //     })
+            // } else if (e.currentTarget === sports_300) {
+            //     sports_300.innerText = ''
+            //     modal_question.innerText = questionObject.Q3.question
+            //     let answer = questionObject.Q3.answer
+            //     let value = questionObject.Q3.value
+            //     openModal()
+            //     submitBtn.addEventListener('click', () => {
+            //         let playerAnswer = document.getElementById('player-answer').value
+            //         if (playerAnswer === answer) {
+            //             modal_question.innerText = 'That\'s Correct!'
+            //             currentScore = currentScore + value
+            //             scoreBoard.innerText = 'Score: ' + currentScore
+            //         } else {
+            //             modal_question.innerText = 'That\'s Incorrect. The correct answer is ' + '"' + answer + '".'
+            //         }
+            //     })
+            // } else if (e.currentTarget === sports_400) {
+            //     sports_400.innerText = ''
+            //     modal_question.innerText = questionObject.Q4.question
+            //     let answer = questionObject.Q4.answer
+            //     let value = questionObject.Q4.value
+            //     openModal()
+            //     submitBtn.addEventListener('click', () => {
+            //         let playerAnswer = document.getElementById('player-answer').value
+            //         if (playerAnswer === answer) {
+            //             modal_question.innerText = 'That\'s Correct!'
+            //             currentScore = currentScore + value
+            //             scoreBoard.innerText = 'Score: ' + currentScore
+            //         } else {
+            //             modal_question.innerText = 'That\'s Incorrect. The correct answer is ' + '"' + answer + '".'
+            //         }
+            //     })
+            // } else if (e.currentTarget === sports_500) {
+            //     sports_500.innerText = ''
+            //     modal_question.innerText = questionObject.Q5.question
+            //     let answer = questionObject.Q5.answer
+            //     let value = questionObject.Q5.value
+            //     openModal()
+            //     submitBtn.addEventListener('click', () => {
+            //         let playerAnswer = document.getElementById('player-answer').value
+            //         if (playerAnswer === answer) {
+            //             modal_question.innerText = 'That\'s Correct!'
+            //             currentScore = currentScore + value
+            //             scoreBoard.innerText = 'Score: ' + currentScore
+            //         } else {
+            //             modal_question.innerText = 'That\'s Incorrect. The correct answer is ' + '"' + answer + '".'
+            //         }
+            //     })
+            // }
         })
     })
 }
 
 startBtn.addEventListener('click', startGame)
-restartBtn.addEventListener('click', startGame)
+// restartBtn.addEventListener('click', startGame)
